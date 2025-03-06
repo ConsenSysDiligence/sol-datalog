b6283e8c62db2fdfb85a2df819364a021773bbda
Nuradil Alymkulov <nurchik17@gmail.com>
2025-03-06 11:36:12 +0600

    Copy compiled program into dist folder

--- scripts/compile_program.ts
+++ scripts/compile_program.ts
@@ -1,3 +1,6 @@
-import { compileDatalog } from "../src/lib/souffle/souffle";
+import { COMPILED_BINARY, compileDatalog } from "../src/lib/souffle/souffle";
+import fs from "fs-extra";
 
 compileDatalog();
+// copy the compiled binary to the dist directory
+fs.copyFileSync(COMPILED_BINARY, COMPILED_BINARY.replace("src", "dist"));
